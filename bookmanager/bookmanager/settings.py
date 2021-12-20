6d5fd3b3707d5d40cf311842f0982c19d2959268
Meari <1262585769@qq.com>
2021-12-20 23:48:36 +0800

    add static dir

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 告诉系统， 静态文件在哪
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
