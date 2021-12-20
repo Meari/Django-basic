8afd488a67821052a062159f11a3c90f5c83811a
Meari <1262585769@qq.com>
2021-12-20 21:46:32 +0800

    add admin站点

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+"""
+1. 模型类需要继承自models.Model
+2. 系统自动为我们添加一个主键---id
+3. 字段
+    字段名=models.类型（选项）
+    字段名其实就是数据表的字段名
+    字段名不要使用python， mysql等的关键字
+"""
+
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10)
+
+    # 重写str方法， 以显示书籍名字
+    def __str__(self):
+        return self.name
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
